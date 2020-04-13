dc422e997de022cc522fbf95dcd7d2a21c62cd85
Caio De Blasio <caio.de-blasio@aklamio.com>
2020-04-13 17:18:16 +0200

    made node server to serve all routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.set("port", process.env.PORT || 8080);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
 var server = app.listen(app.get("port"), function () {
   console.log("listening on port ", server.address().port);
 });
